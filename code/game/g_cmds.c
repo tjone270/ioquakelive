b8d93251dbe24d191a68f38f1be77afe53f5439f
Thomas Jones <me@thomasjones.id.au>
2025-04-14 19:39:12 +1000

    Add `cointoss` vote.

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -1287,9 +1287,10 @@ void Cmd_CallVote_f(gentity_t* ent) {
     } else if (!Q_stricmp(arg1, "g_doWarmup")) {
     } else if (!Q_stricmp(arg1, "timelimit")) {
     } else if (!Q_stricmp(arg1, "fraglimit")) {
+    } else if (!Q_stricmp(arg1, "cointoss")) {
     } else {
         trap_SendServerCommand(ent - g_entities, "print \"Invalid vote string.\n\"");
-        trap_SendServerCommand(ent - g_entities, "print \"Vote commands are: map_restart, nextmap, map <mapname>, g_gametype <n>, kick <player>, clientkick <clientnum>, g_doWarmup, timelimit <time>, fraglimit <frags>.\n\"");
+        trap_SendServerCommand(ent - g_entities, "print \"Vote commands are: map_restart, nextmap, map <mapname>, g_gametype <n>, kick <player>, clientkick <clientnum>, g_doWarmup, timelimit <time>, fraglimit <frags>, cointoss.\n\"");
         return;
     }
 
@@ -1350,6 +1351,9 @@ void Cmd_CallVote_f(gentity_t* ent) {
 
         Com_sprintf(level.voteString, sizeof(level.voteString), "clientkick %d", i);
         Com_sprintf(level.voteDisplayString, sizeof(level.voteDisplayString), "kick %s", level.clients[i].pers.netname);
+	} else if (!Q_stricmp(arg1, "cointoss")) {
+        Com_sprintf(level.voteString, sizeof(level.voteString), "cointoss\n");
+        Com_sprintf(level.voteDisplayString, sizeof(level.voteDisplayString), "cointoss", level.clients[i].pers.netname);
     } else {
         Com_sprintf(level.voteString, sizeof(level.voteString), "%s \"%s\"", arg1, arg2);
         Com_sprintf(level.voteDisplayString, sizeof(level.voteDisplayString), "%s", level.voteString);
